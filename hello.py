7e1bfbeca64f099f028665c48edf3199ef256fb8
Kevin Gao <kgao1997@gmail.com>
2022-12-17 17:28:43 -0500

    basic flask pipeline established

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+#Render a basic HTML Template in Jinja
+def hello():
+    return render_template('index.html')
+#Test to show flask working
+'''def hello_world():
     return 'Hello, World!'
+'''
