9146fe4f3eecc1709e32e730f90767a795a91c50
Kevin Gao <kgao1997@gmail.com>
2022-12-20 12:44:55 -0500

    basic routing implemented

--- hello.py
+++ hello.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 #Render a basic HTML Template in Jinja
 def hello():
+    return render_template('index.html')
+
+@app.route('/profile')
+def profile(): 
+    return render_template('profile.html')
+
+@app.route('/results')
+def results(): 
     return render_template('results.html')
+
+@app.route('/posting')
+def posting(): 
+    return render_template('posting.html')
 #Test to show flask working
 '''def hello_world():
     return 'Hello, World!'
