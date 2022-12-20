23cffcdecd3727f641e1e760ca2b375962298af1
Kevin Gao <kgao1997@gmail.com>
2022-12-19 22:52:01 -0500

    profile page done!

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 #Render a basic HTML Template in Jinja
 def hello():
-    return render_template('index.html')
+    return render_template('profile.html')
 #Test to show flask working
 '''def hello_world():
     return 'Hello, World!'
